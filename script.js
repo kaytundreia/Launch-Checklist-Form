c4e0a76370d36c4ef40593bd2dd169d8686673ba
kaytundreia <74748908+kaytundreia@users.noreply.github.com>
2020-12-14 20:01:51 -0600

    update to javascript

--- script.js
+++ script.js
@@ -11,3 +11,24 @@
 </ol>
 <img src="${}">
 */
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]").value
+      let copilotName = document.querySelector("input[name=copilotName]").value;
+      let fuelLevel = parseInt(document.querySelector("input[name=fuelLevel]").value);
+      let cargoMass = parseInt(document.querySelector("input[name=cargoMass]").value);
+      event.preventDefault();
+      if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === "") {
+         alert("All fields are required!");
+      } else if (typeof pilotName !== "string") {
+         alert("Pilot Name must be a word."); 
+      } else if (typeof copilotName !== "string") {
+         alert("Co-Pilot name must be a word.");
+      } else if (isNaN(fuelLevel) === true) {
+         alert("Fuel Level must be a number.");
+      } else if (isNaN(cargoMass) === true) {
+         alert("Cargo Mass must be a number.")
+      }
+   });
+});
